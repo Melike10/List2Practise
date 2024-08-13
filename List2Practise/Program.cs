b706fcad2c8e197fec672f6d8bae1fc262d53a70
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-13 20:06:49 +0300

    Add project files.

---
+++ List2Practise/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> kahveler = new List<string>();
+
+// we  take the coffee's name from users and add the list
+for(int i = 1; i < 6; i++)
+{
+    Console.Write("Kahve "+i+": ");
+    kahveler.Add(Console.ReadLine());
+}
+
+// print the coffee list
+Console.WriteLine("----Girilen Kahve İsimleri----");
+foreach (string k in kahveler)
+{
+    Console.WriteLine(k);
+}
